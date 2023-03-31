90d293ebd80232a6a69661f96308ee020dd55a1d
Bharath-1991 <bharathkumar.subramani@outlook.com>
2023-03-31 12:49:06 -0400

    Changes for control tower through github actions

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "bh-infra-state-bucket"
+    bucket         = "bh-infra-state-bucket1"
     region         = "ca-central-1"
     key            =  "infra/control/bh-infra-ct"
-    dynamodb_table = "bh-infra-file-state-dynamo"
+    dynamodb_table = "bh-infra-file-state1-dynamo1"
     encrypt        = true
   }
 }
