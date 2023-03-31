29a1e0c58ab1d08d071b39524b41723732d88476
Bharath-1991 <bharathkumar.subramani@outlook.com>
2023-03-31 15:19:35 -0400

    Changing backend details

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "control-tower-infra-bucket"
     region         = "ca-central-1"
-    key            =  "infra/control/infra-ct"
+    key            =  "infra/control/infra-ct1"
     dynamodb_table = "control-tower-dynamo-backend"
     encrypt        = true
   }
