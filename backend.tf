bd9734277fd2e5dbd7ce13f54f6654f1c4fc9b05
Bharath-1991 <bharathkumar.subramani@outlook.com>
2023-03-31 14:30:32 -0400

    Test Temp changes

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "bh-infra-state-bucket1"
+    bucket         = "bh-infra-state1-bucket"
     region         = "ca-central-1"
     key            =  "infra/control/bh-infra-ct"
     dynamodb_table = "bh-infra-file-state1-dynamo1"
