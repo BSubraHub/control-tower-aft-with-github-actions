b49e3617c3a24b400f0c0200548ff68a38943d3e
Bharath-1991 <bharathkumar.subramani@outlook.com>
2023-03-31 11:59:24 -0400

    Changes for github self host runners

--- main.tf
+++ main.tf
@@ -28,3 +28,27 @@ resource "aws_iam_role" "test_role" {
     tag-key = "tag-value"
   }
 }
+
+resource "aws_iam_role" "test_role1233" {
+  name = "test_role_created_by_bharath_on_mar31_at12PM"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
