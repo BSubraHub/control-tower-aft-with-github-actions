98994478cf9200d6678042de3a68f933fcfb87f7
Bharath-1991 <bharathkumar.subramani@outlook.com>
2023-04-03 15:21:18 -0400

    Added Aws credential chain

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "control-tower-infra-bucket1"
+    bucket         = "control-tower-infra-bucket"
     region         = "ca-central-1"
     key            =  "infra/control/infra-control-tower"
-    dynamodb_table = "control-tower-manage-dynamo-backend1"
+    dynamodb_table = "control-tower-manage-dynamo-backend"
     encrypt        = true
     # access_key = "AKIAYIR3MAAI6CQG4HWY"
     # secret_key = "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"
--- self-host-runner-ECS/backend.tf
+++ self-host-runner-ECS/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "Aft-github-runner-state-bucket1"
+    bucket         = "Aft-github-runner-state-bucket"
     region         = "ca-central-1"
     key            =  "infra/aft/github-runner"
-    dynamodb_table = "bharath-runner-file-state-dynamo1"
+    dynamodb_table = "bharath-runner-file-state-dynamo"
     encrypt        = true
     # access_key = "AKIAYIR3MAAI6CQG4HWY"
     # secret_key = "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"
