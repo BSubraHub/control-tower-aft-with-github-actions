1afbe37b886edcd02df780381bd3acb1cfaddd60
Bharath-1991 <bharathkumar.subramani@outlook.com>
2023-04-03 09:32:06 -0400

    Changing backend details

--- backend.tf
+++ backend.tf
@@ -2,8 +2,8 @@ terraform {
   backend "s3" {
     bucket         = "control-tower-infra-bucket"
     region         = "ca-central-1"
-    key            =  "infra/control/infra-ct2"
-    dynamodb_table = "control-tower-dynamo-backend"
+    key            =  "infra/control/infra-control-tower"
+    dynamodb_table = "control-tower-manage-dynamo-backend"
     encrypt        = true
     access_key = "AKIAYIR3MAAI6CQG4HWY"
     secret_key = "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"
--- self-host-runner-ECS/backend.tf
+++ self-host-runner-ECS/backend.tf
@@ -2,8 +2,8 @@ terraform {
   backend "s3" {
     bucket         = "bh-github-runner-state-bucket"
     region         = "ca-central-1"
-    key            =  "infra/control/bh-github-runner-ct1"
-    dynamodb_table = "bh-runner-file-state-dynamo"
+    key            =  "infra/control/bharath-github-runner"
+    dynamodb_table = "bharath-runner-file-state-dynamo"
     encrypt        = true
     access_key = "AKIAYIR3MAAI6CQG4HWY"
     secret_key = "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"
