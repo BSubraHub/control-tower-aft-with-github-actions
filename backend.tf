ca956894164839fcd2e2cb1b9bc5068a3fc42bc2
Bharath-1991 <bharathkumar.subramani@outlook.com>
2023-04-03 08:55:03 -0400

    Changing backend details

--- backend.tf
+++ backend.tf
@@ -5,6 +5,8 @@ terraform {
     key            =  "infra/control/infra-ct2"
     dynamodb_table = "control-tower-dynamo-backend"
     encrypt        = true
+    access_key = "AKIAYIR3MAAI6CQG4HWY"
+    secret_key = "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"
   }
 }
 
--- self-host-runner-ECS/backend.tf
+++ self-host-runner-ECS/backend.tf
@@ -5,6 +5,8 @@ terraform {
     key            =  "infra/control/bh-github-runner-ct"
     dynamodb_table = "bh-runner-file-state-dynamo"
     encrypt        = true
+    access_key = "AKIAYIR3MAAI6CQG4HWY"
+    secret_key = "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"
   }
 }
 
