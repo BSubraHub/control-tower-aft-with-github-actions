8c81d52b2c1ddadf1469a7afa6e133c8327f4154
Bharath-1991 <bharathkumar.subramani@outlook.com>
2023-04-04 13:59:39 -0400

    April 4 Added Aws credential chain

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "control-tower-infra-bucket"
     region         = "ca-central-1"
     key            =  "infra/control/infra-control-tower"
-    dynamodb_table = "control-tower-manage-dynamo-backend"
+    dynamodb_table = "control-tower-manage-dynamo-backend1"
     encrypt        = true
     # access_key = "AKIAYIR3MAAI6CQG4HWY"
     # secret_key = "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"
