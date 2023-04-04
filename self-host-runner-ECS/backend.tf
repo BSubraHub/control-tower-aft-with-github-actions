42e75e9be16963167b9efbe09e6a2d0db51c02fd
Bharath-1991 <bharathkumar.subramani@outlook.com>
2023-04-04 09:37:49 -0400

    April 4 Added Aws credential chain

--- self-host-runner-ECS/backend.tf
+++ self-host-runner-ECS/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "Aft-github-runner-state-bucket"
+    bucket         = "aft-github-runner-state-bucket"
     region         = "ca-central-1"
     key            =  "infra/aft/github-runner"
     dynamodb_table = "bharath-runner-file-state-dynamo"
